c7e7d62e7bb2393fcb0f5684bcb5b3d70b03bfae
adidi3 <ap0413@bs.amalnet.k12.il>
2018-02-19 14:33:48 +0200

    1st commit

--- app/src/main/java/com/example/adipe/adi0001/MainActivity.java
+++ app/src/main/java/com/example/adipe/adi0001/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.adipe.adi0001;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    Switch rl;
+    ToggleButton ll;
+    LinearLayout rr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rl = (Switch) findViewById(R.id.switch1);
+        ll = (ToggleButton) findViewById(R.id.toggleButton);
+        rr=(LinearLayout) findViewById(R.id.roro);
 
+
+
+
+
+
+
+    }
+
+    public void switcher(View view) {
+
+        if (rl.isChecked()) {
+            rr.setBackgroundColor(Color.RED);
+        }
+        else rr.setBackgroundColor(Color.BLUE);
+
+
+
+
+
+    }
+
+    public void toggle(View view) {
+        if (ll.isChecked()) {
+            rr.setBackgroundColor(Color.YELLOW);
+        } else rr.setBackgroundColor(Color.GREEN);
     }
 }
